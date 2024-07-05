20b4dbae30b34e167d134fc7306bb4fa62bf367d
mlvenkey <mlvenkateswra9@gmail.com>
2024-07-05 12:36:47 +0000

    second commit

---
+++ dgos.js
@@ -0,0 +1,10 @@
+this  file contains java scripting
+mainly contains angular java it ,= may acts the crearence angular activity 
+cgwieu]
+wuviuwv
+]
+shvjbivc
+csdvbs
+
+
+cdsjhcbdscjesn
---
+++ venkat.py
@@ -0,0 +1,12 @@
+s file comntsins python scripting'
+
+
+
+
+wefgeqrj
+fhvhrhreqifhqrw
+feshcbvureev
+cdsjvnrvqoof
+fewfhwefnfjf
+fjrrfirejfrhjfkiew
+fewenrviewe
